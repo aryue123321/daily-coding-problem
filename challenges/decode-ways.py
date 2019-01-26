465b68ea60b4c126943456bee5b8c8b0f8ea8daf
aryue123321 <44553958+aryue123321@users.noreply.github.com>
2019-01-25 21:39:03 -0800

    Update decode-ways.py
    constant space

--- challenges/decode-ways.py
+++ challenges/decode-ways.py
@@ -10,26 +10,36 @@ You can assume that the messages are decodable. For example, '001' is not allowe
 
 """
 
-allMessages = []
+# allMessages = []
 
-def decode(parsed, remaining):
-    if remaining == "":
-        if parsed not in allMessages:
-            allMessages.append(parsed)
-        else:
-            print(parsed)
-        return
+# def decode(parsed, remaining):
+#     if remaining == "":
+#         if parsed not in allMessages:
+#             allMessages.append(parsed)
+#         else:
+#             print(parsed)
+#         return
 
-    singleDigit = int(remaining[0])
-    if singleDigit in range(1,27):
-        decode(parsed+[singleDigit], remaining[1:])
+#     singleDigit = int(remaining[0])
+#     if singleDigit in range(1,27):
+#         decode(parsed+[singleDigit], remaining[1:])
 
-    if len(remaining)>1:
-        twoDigits = int(remaining[0:2])
-        if twoDigits in range(1,27):
-            decode(parsed+[twoDigits], remaining[2:])
+#     if len(remaining)>1:
+#         twoDigits = int(remaining[0:2])
+#         if twoDigits in range(1,27):
+#             decode(parsed+[twoDigits], remaining[2:])
 
+ def decode(L):
+    if len(L) == 0:
+        return 1
+    oneDigit = decode(L[1:]) if int(L[:1]) in range(1, 27) else 0
+    twoDigit = 0
+    if (len(L) > 1):
+        twoDigit = decode(L[2:]) if int(L[:2]) in range(1, 27) else 0
+    return oneDigit + twoDigit           
+ 
 if __name__ == "__main__":
     message = input().strip()
-    decode([], message)
-    print(len(allMessages))
+    print(decodeAgain(message))
+#     decode([], message)
+#     print(len(allMessages))
